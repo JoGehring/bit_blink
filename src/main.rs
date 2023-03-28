d1ec2f894f8b2b59555e8b27eb583f30f176d1a3
JoGehring <92168368+JoGehring@users.noreply.github.com>
2023-03-28 17:30:59 +0200

    change initial window to better example

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,49 @@
-use libadwaita::Application;
-use libadwaita::prelude::ApplicationExt;
-use libadwaita::prelude::ApplicationExtManual;
-use libadwaita::ApplicationWindow;
-use libadwaita::prelude::GtkWindowExt;
+use libadwaita::prelude::*;
 
-pub fn main() {
-    let application = Application::new(Some("com.example.gtk-rss-reader"), Default::default());
+use libadwaita::{ActionRow, Application, ApplicationWindow, HeaderBar};
+use libadwaita::gtk::{Box, ListBox, Orientation, SelectionMode};
 
-    application.connect_activate(build_ui);
-    application.run();
-}
-
-fn build_ui(app: &Application) {
-    // Create a window and set the title
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("My GTK App")
+fn main() {
+    let application = Application::builder()
+        .application_id("com.example.FirstAdwaitaApp")
         .build();
 
-    // Present window
-    window.present();
+    application.connect_activate(|app| {
+        // ActionRows are only available in Adwaita
+        let row = ActionRow::builder()
+            .activatable(true)
+            .title("Click me")
+            .build();
+        row.connect_activated(|_| {
+            eprintln!("Clicked!");
+        });
+
+        let list = ListBox::builder()
+            .margin_top(32)
+            .margin_end(32)
+            .margin_bottom(32)
+            .margin_start(32)
+            .selection_mode(SelectionMode::None)
+            // makes the list look nicer
+            .css_classes(vec![String::from("boxed-list")])
+            .build();
+        list.append(&row);
+
+        // Combine the content in a box
+        let content = Box::new(Orientation::Vertical, 0);
+        // Adwaitas' ApplicationWindow does not include a HeaderBar
+        content.append(&HeaderBar::new());
+        content.append(&list);
+
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("First App")
+            .default_width(350)
+            // add content to window
+            .content(&content)
+            .build();
+        window.show();
+    });
+
+    application.run();
 }
