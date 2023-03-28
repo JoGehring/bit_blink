31b5991f22443d774059e722caa0674580526e13
JoGehring <92168368+JoGehring@users.noreply.github.com>
2023-03-28 16:50:25 +0200

    create first window with libadwaita

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use libadwaita::Application;
+use libadwaita::prelude::ApplicationExt;
+use libadwaita::prelude::ApplicationExtManual;
+use libadwaita::ApplicationWindow;
+use libadwaita::prelude::GtkWindowExt;
+
+pub fn main() {
+    let application = Application::new(Some("com.example.gtk-rss-reader"), Default::default());
+
+    application.connect_activate(build_ui);
+    application.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .build();
+
+    // Present window
+    window.present();
 }
